861a3d680a8d068c42580912bc10f531c3bbbf79
Zarnerz <holstein30@live.com>
2018-07-29 20:35:27 -0400

    video list & items

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = "AIzaSyC3NGcxFBdhW9lb3sVz2i8ROQsXBtx2znQ";
 
@@ -22,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
